39847cf34487caea3a356c4ed5c3aaa47fd25db2
Prio_Prastyo <74877643+prioprasty@users.noreply.github.com>
2022-09-11 11:44:38 +0700

    Update donasi.cjs

--- plugins/donasi.cjs
+++ plugins/donasi.cjs
@@ -1,11 +1,21 @@
 var handler = async m => m.reply(`
-╭─「 Donasi 」
-│ • TELKOMSEL [085770598246]
+╭─「 Donasi Via  」
+│ • INDOSAT [085770598246]
 │ • DANA [085770598246]
 │ • OVO [085770598246]
 │ • GOPAY [085770598246]
 │ • SHOPPY PAY [085770598246]
 ╰────
+Donasi seikhlasnya walaupun hanya se persen pun
+Tetap saya terima, karena donasi kamu berasal dari
+Hati dan niat yang sangat baik. . .
+
+Saya sangat berterima kasih bagi kamu yang sudah
+Berdonasi kepada saya, saya mendoakan anda agar
+*Amal* dan *Ibadah* anda senantiasa diterima di
+Sisi tuhan ( Allah SWT ) selalu
+Aamiin yaa rabbal allamiin. . .
+
 ╭─「 Hubungi 」
 │ > Ingin donasi? Wa.me/6285770598246
 ╰────
